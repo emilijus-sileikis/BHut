1c4b5a86283b8821bbcef17384d45542adaa87c5
Emilijus <emilijus.sileikis@mif.stud.vu.lt>
2023-02-18 11:22:43 +0200

    Admin middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +26,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function authenticated() {
+        if (Auth::user()->role_as == '1') {
+            return redirect('admin/dashboard')->with('status', 'Welcome to Dashboard!');
+        }
+        else {
+            return redirect('/home')->with('status', 'Logged In Successfully!');
+        }
+    }
 
     /**
      * Create a new controller instance.
